0e42c364ca4fe2a84392cd05f4b7232d582e5866
Augusto Hansel <augustohansel@hotmail.com>
2022-05-03 15:20:14 -0300

    add test to backend

--- tests/factories/createUserFactory.ts
+++ tests/factories/createUserFactory.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import { prisma } from "../../src/database.js";
 import { CreateUserData } from "../../src/services/userService.js"
 
-export default async function userFactory(user: CreateUserData) {
+export default async function createUserFactory(user: CreateUserData) {
   await prisma.user.create({
     data: {
       ...user,
